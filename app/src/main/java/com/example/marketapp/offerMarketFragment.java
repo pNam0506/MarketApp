6579913eff4e89c174951fe691040e81d74d4620
pNam0506 <134200598+pNam0506@users.noreply.github.com>
2023-05-22 22:03:38 +0700

    Update offerMarketFragment.java

--- app/src/main/java/com/example/marketapp/offerMarketFragment.java
+++ app/src/main/java/com/example/marketapp/offerMarketFragment.java
@@ -1,64 +1,25 @@
 package com.example.marketapp;
+import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
-import android.view.LayoutInflater;
+import android.os.Handler;
 import android.view.View;
-import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.ScaleAnimation;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link offerMarketFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class offerMarketFragment extends Fragment {
+public class offerMarketFragment extends AppCompatActivity {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    // number of selected tab, we have 4 tabs so value must lie between 1-4
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public offerMarketFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment offerMarketFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static offerMarketFragment newInstance(String param1, String param2) {
-        offerMarketFragment fragment = new offerMarketFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+        setContentView(R.layout.fragment_offer_market);
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_offer_market, container, false);
     }
 }
