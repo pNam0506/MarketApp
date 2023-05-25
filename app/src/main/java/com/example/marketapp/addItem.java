db1507b847af6453ecfcbdc8de01bfed65b09e3f
pNam0506 <134200598+pNam0506@users.noreply.github.com>
2023-05-25 23:04:05 +0700

    หน้ากฏของตลาด

---
+++ app/src/main/java/com/example/marketapp/addItem.java
@@ -0,0 +1,15 @@
+package com.example.marketapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class addItem extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_item);
+
+    }
+}
