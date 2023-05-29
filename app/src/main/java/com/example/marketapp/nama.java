b7961f2cc49275328228a8d3de2f246f28ea53c8
pNam0506 <134200598+pNam0506@users.noreply.github.com>
2023-05-29 17:21:01 +0700

    Map ตลาด

---
+++ app/src/main/java/com/example/marketapp/nama.java
@@ -0,0 +1,14 @@
+package com.example.marketapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class nama extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nama);
+    }
+}
