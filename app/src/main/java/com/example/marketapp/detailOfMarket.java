4a801601f3242109135dfd225ef3aecedd9caa16
pNam0506 <134200598+pNam0506@users.noreply.github.com>
2023-06-04 16:13:53 +0700

    เปลี่ยนที่อยู่ google map

--- app/src/main/java/com/example/marketapp/detailOfMarket.java
+++ app/src/main/java/com/example/marketapp/detailOfMarket.java
@@ -122,7 +122,7 @@ public class detailOfMarket extends AppCompatActivity {
                 @Override
                 public void onClick(View v) {
 
-                    Uri uri = Uri.parse("https://www.google.co.th/maps/dir/13.9084904,100.5059896/%E0%B8%96.+%E0%B8%9E%E0%B8%A3%E0%B8%B0%E0%B8%A3%E0%B8%B2%E0%B8%A1+9+%E0%B9%80%E0%B8%82%E0%B8%95%E0%B8%AB%E0%B9%89%E0%B8%A7%E0%B8%A2%E0%B8%82%E0%B8%A7%E0%B8%B2%E0%B8%87+%E0%B8%81%E0%B8%A3%E0%B8%B8%E0%B8%87%E0%B9%80%E0%B8%97%E0%B8%9E%E0%B8%A1%E0%B8%AB%E0%B8%B2%E0%B8%99%E0%B8%84%E0%B8%A3/@13.8309344,100.4642671,12z/data=!3m1!4b1!4m9!4m8!1m1!4e1!1m5!1m1!1s0x30e29e3f19a81f03:0xbf2e74f9fb8ae289!2m2!1d100.5843957!2d13.7542289?hl=th&entry=ttu");
+                    Uri uri = Uri.parse("https://www.google.co.th/maps/dir/13.9084609,100.5059913/%E0%B9%81%E0%B8%82%E0%B8%A7%E0%B8%87%E0%B8%97%E0%B8%B8%E0%B9%88%E0%B8%87%E0%B8%A1%E0%B8%AB%E0%B8%B2%E0%B9%80%E0%B8%A1%E0%B8%86+%E0%B9%80%E0%B8%82%E0%B8%95%E0%B8%AA%E0%B8%B2%E0%B8%97%E0%B8%A3+%E0%B8%81%E0%B8%A3%E0%B8%B8%E0%B8%87%E0%B9%80%E0%B8%97%E0%B8%9E%E0%B8%A1%E0%B8%AB%E0%B8%B2%E0%B8%99%E0%B8%84%E0%B8%A3+10120/@13.8139044,100.4642671,12z/data=!3m1!4b1!4m10!4m9!1m1!4e1!1m5!1m1!1s0x30e29f39c444f213:0x40100b25de28e10!2m2!1d100.543675!2d13.7198011!3e0?entry=ttu");
                     Intent intent = new Intent(Intent.ACTION_VIEW,uri);
                     intent.setPackage("com.google.android.apps.maps");
                     intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
@@ -152,7 +152,37 @@ public class detailOfMarket extends AppCompatActivity {
                 @Override
                 public void onClick(View v) {
 
-                    Uri uri = Uri.parse("https://www.google.co.th/maps/dir/13.9084904,100.5059896/%E0%B8%96.+%E0%B8%9E%E0%B8%A3%E0%B8%B0%E0%B8%A3%E0%B8%B2%E0%B8%A1+9+%E0%B9%80%E0%B8%82%E0%B8%95%E0%B8%AB%E0%B9%89%E0%B8%A7%E0%B8%A2%E0%B8%82%E0%B8%A7%E0%B8%B2%E0%B8%87+%E0%B8%81%E0%B8%A3%E0%B8%B8%E0%B8%87%E0%B9%80%E0%B8%97%E0%B8%9E%E0%B8%A1%E0%B8%AB%E0%B8%B2%E0%B8%99%E0%B8%84%E0%B8%A3/@13.8309344,100.4642671,12z/data=!3m1!4b1!4m9!4m8!1m1!4e1!1m5!1m1!1s0x30e29e3f19a81f03:0xbf2e74f9fb8ae289!2m2!1d100.5843957!2d13.7542289?hl=th&entry=ttu");
+                    Uri uri = Uri.parse("https://www.google.co.th/maps/dir/13.9084609,100.5059913/Ratchada+Tha+Phra+Condominium,+2+Ratchadaphisek+Rd,+Talat+Phlu,+Thon+Buri,+Bangkok+10600/@13.8196875,100.4142343,12z/data=!3m1!4b1!4m9!4m8!1m1!4e1!1m5!1m1!1s0x30e2985b8883659f:0xe37c1f0d525724aa!2m2!1d100.4770302!2d13.7199479?entry=ttu");
+                    Intent intent = new Intent(Intent.ACTION_VIEW,uri);
+                    intent.setPackage("com.google.android.apps.maps");
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    startActivity(intent);
+
+                }
+            });
+
+
+
+
+
+        }
+
+        if(bundle.getString("NameMk").equals("Lingko")) {
+
+            Map.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(detailOfMarket.this, tuomom.class);
+                    startActivity(intent);
+                }
+
+            });
+
+            direc.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                    Uri uri = Uri.parse("https://www.google.co.th/maps/dir/13.9084609,100.5059913/39+Petchkasem+Rd,+Khwaeng+Bang+Khae,+Khet+Bang+Khae,+Krung+Thep+Maha+Nakhon+10160/@13.8200876,100.3814165,12z/data=!3m1!4b1!4m10!4m9!1m1!4e1!1m5!1m1!1s0x30e297c3dfe53025:0xcc720e9747a14fbf!2m2!1d100.413088!2d13.7110888!3e0?entry=ttu");
                     Intent intent = new Intent(Intent.ACTION_VIEW,uri);
                     intent.setPackage("com.google.android.apps.maps");
                     intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
